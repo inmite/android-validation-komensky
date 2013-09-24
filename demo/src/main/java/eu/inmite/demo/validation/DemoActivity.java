db6b86178ed31d8a68b53bb7de190dfb8734a7eb
Tomas Vondracek <tomas.vondracek@gmail.com>
2013-09-24 19:28:39 +0200

    demo app structure

---
+++ demo/src/main/java/eu/inmite/demo/validation/DemoActivity.java
@@ -0,0 +1,14 @@
+package eu.inmite.demo.validation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Tomas Vondracek
+ */
+public class DemoActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
